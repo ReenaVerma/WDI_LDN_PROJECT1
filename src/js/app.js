0c1ce3d5aae5d97525f0d4a296e3e040a2817b00
ReenaVerma <reena_verma@hotmail.com>
2018-02-09 15:38:51 +0000

    basic html and start of js

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,75 @@
+$('#play');
+$('#countdown');
 
+let timeRemaining = 10;
+let timerOn = false;
+let timerID = null; //null is the same as false
+
+
+function init() {
+
+  console.log('hello');
+
+
+  play.addEventListener('click', () => {
+
+    if (!timerOn) {
+      timerOn = true;
+      // answers.classList.remove('hide');
+      // redBtn.classList.add('hide');
+      // // questions
+      // questions.innerHTML = `${a} + ${b}`;
+
+      // WHEN CLICKED START THE COUNTDOWN FROM 10
+      function countdownLogic () {
+
+        { if (timeRemaining > 0) {
+            --timeRemaining;
+          display.innerHTML = timeRemaining;
+          }
+          // IF COUNTDOWN HITS ZERO ADD CLASS
+
+          if (timeRemaining === 0) {
+            play.classList.remove('hide');
+            // document.body.appendChild('redBtn');    // Append <button> to <body>
+          } else {
+            play.classList.remove('ringing');
+          }
+
+
+        }
+      }
+
+      timerId = setInterval(countdownLogic, 1000);
+
+    } else {
+      timerOn = false;
+      clearInterval(timerId);
+
+    }
+    redBtn.innerHTML = 'PLAY AGAIN!';
+  });
+
+
+
+}
+
+window.addEventListener('load', init);
+
+// Build basic html structure
+// Pull through variables
+
+// Images sit within grid
+// Images have a disort class.
+
+// When 'play' load page with images
+// Loaded images sliced
+
+
+// Page loads with timer.
+// Page loads with points score.
+
+
+// Level one.  Simple guess vs timer game.
+
+// Level two.  The tiles start to drop off and deducts 1pt per tile.
