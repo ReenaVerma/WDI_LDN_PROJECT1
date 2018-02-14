bf264fef260a613f6286efd5ea2e6cc301eaf220
ReenaVerma <reena_verma@hotmail.com>
2018-02-14 17:16:37 +0000

    end of day

--- src/js/app.js
+++ src/js/app.js
@@ -20,10 +20,14 @@ function init() {
   const $yodasound = $('#yodasound');
   const $alienssound = $('#alienssound');
   const $idsound = $('#idsound');
+  const $etsound = $('#etsound');
+  const $matrixSound = $('#matrixsound');
+  const $terminatorsound = $('#terminatorsound');
 
 
 
-  let timeRemaining = 30;
+
+  let timeRemaining = 60;
   let timerOn = false;
   let totalpoints = 0;
   let clock = null; //null is the same as false
@@ -44,18 +48,46 @@ function init() {
     sound: $yodasound
 
   }, {
-    answer: 'aliens',
-    images: ['images/alien1.jpg', 'images/alien2.jpg', 'images/alien3.jpg', 'images/alien4.jpg', 'images/alien5.jpg', 'images/alien6.jpg', 'images/alien7.jpg', 'images/alien8.jpg', 'images/alien9.jpg', 'images/alien10.jpg', 'images/alien11.jpg', 'images/alien12.jpg'],
+    answer: 'et',
+    images: ['images/et1.jpg', 'images/et2.jpg', 'images/et3.jpg', 'images/et4.jpg', 'images/et5.jpg', 'images/et6.jpg', 'images/et7.jpg', 'images/et8.jpg', 'images/et9.jpg', 'images/et10.jpg', 'images/et11.jpg', 'images/et12.jpg' ],
     level: 2,
     answerimage: ['images/aliens1.jpg'],
+    sound: $etsound
+
+  },{
+    answer: 'the matrix',
+    images: ['images/matrix1.jpg', 'images/matrix2.jpg', 'images/matrix3.jpg', 'images/matrix4.jpg', 'images/matrix5.jpg', 'images/matrix6.jpg', 'images/matrix7.jpg', 'images/matrix8.jpg', 'images/matrix9.jpg', 'images/matrix10.jpg', 'images/matrix11.jpg', 'images/matrix12.jpg' ],
+    level: 3,
+    answerimage: ['images/aliens1.jpg'],
+    sound: $matrixSound
+
+  },{
+    answer: 'flash gordon',
+    images: ['images/flash1.jpg', 'images/flash2.jpg', 'images/flash3.jpg', 'images/flash4.jpg', 'images/flash5.jpg', 'images/flash6.jpg', 'images/flash7.jpg', 'images/flash8.jpg', 'images/flash9.jpg', 'images/flash10.jpg', 'images/flash11.jpg', 'images/flash12.jpg' ],
+    level: 4,
+    answerimage: ['images/aliens1.jpg'],
     sound: $alienssound
+
+  },{
+    answer: 'the terminator',
+    images: ['images/t1.jpg', 'images/t2.jpg', 'images/t3.jpg', 'images/t4.jpg', 'images/t5.jpg', 'images/t6.jpg', 'images/t7.jpg', 'images/t8.jpg', 'images/t9.jpg', 'images/t10.jpg', 'images/t11.jpg', 'images/t12.jpg' ],
+    level: 5,
+    answerimage: ['images/aliens1.jpg'],
+    sound: $terminatorsound
 
   }, {
     answer: 'independence day',
     images: ['images/id1.jpg', 'images/id2.jpg', 'images/id3.jpg', 'images/id4.jpg', 'images/id5.jpg', 'images/id6.jpg', 'images/id7.jpg', 'images/id8.jpg', 'images/id9.jpg', 'images/id10.jpg', 'images/id11.jpg', 'images/id12.jpg'],
-    level: 3,
+    level: 7,
     answerimage: ['images/id1.jpg'],
     sound: $idsound
+  }, {
+    answer: 'aliens',
+    images: ['images/alien1.jpg', 'images/alien2.jpg', 'images/alien3.jpg', 'images/alien4.jpg', 'images/alien5.jpg', 'images/alien6.jpg', 'images/alien7.jpg', 'images/alien8.jpg', 'images/alien9.jpg', 'images/alien10.jpg', 'images/alien11.jpg', 'images/alien12.jpg'],
+    level: 6,
+    answerimage: ['images/aliens1.jpg'],
+    sound: $alienssound
+
   }];
 
 
@@ -154,10 +186,16 @@ function init() {
     console.log('submitted');
     const answer = useranswer.val().toLowerCase();
 
+
+    let str = (game[round].answer);
+    str = str.toLowerCase().replace(/\b[a-z]/g, function(letter) {
+      return letter.toUpperCase();
+    });
+
     // LOGIC FOR IF THE ANSWER IS CORRECT
     if (answer === game[round].answer) {
       console.log('answer true');
-      $placeholder.html('You got it!  The movie was: ' + game[round].answer);
+      $placeholder.html('You got it!  The movie was: ' + '<br>' + str);
       totalpoints += 1;
 
       $images.empty();
